ac6a85d8693567544d6d91c731f8af3679cda6e3
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2017-03-21 00:34:44 +0000

    patch: create_deck v1

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,22 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def create_deck do 
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    
+    cards = for value <- values do 
+      for suit <- suits do 
+        "#{value} of #{suit}"
+      end
+    end 
+    
+    List.flatten(cards)
+  end
+  
+  def shuffle(deck) do 
+    Enum.shuffle(deck)
+  end
+  
+  def contains?(deck, card) do 
+    Enum.member?(deck,card)
   end
 end
